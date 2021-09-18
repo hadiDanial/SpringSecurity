41ec94d8920494210971e576ef5d345e4b62807c
Hadi <hadidanial97@gmail.com>
2021-09-18 12:35:42 +0300

    Update User.java

--- Backend/src/main/java/hadi/springSecurity/beans/entities/User.java
+++ Backend/src/main/java/hadi/springSecurity/beans/entities/User.java
@@ -27,5 +27,121 @@ public class User
 	private Name name;
 	
 	private String password;
+
+	public User() {	}
+
+	public User(long id, String username, String email, Name name, String password)
+	{
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public Name getName()
+	{
+		return name;
+	}
+
+	public void setName(Name name)
+	{
+		this.name = name;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	public long getId()
+	{
+		return id;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null)
+		{
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null)
+		{
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null)
+		{
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null)
+		{
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", name=" + name.toString() + "]";
+	}
+	
 	
 }
