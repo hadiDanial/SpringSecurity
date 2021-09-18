d6af203c063eb27e1e0019599b9d7b2427758900
Hadi <hadidanial97@gmail.com>
2021-09-18 13:26:50 +0300

    Create SecurityUser.java

---
+++ Backend/src/main/java/hadi/springSecurity/security/model/SecurityUser.java
@@ -0,0 +1,62 @@
+package hadi.springSecurity.security.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import hadi.springSecurity.beans.entities.User;
+
+public class SecurityUser implements UserDetails
+{
+	private final User user;
+	
+	public SecurityUser(User user)
+	{
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword()
+	{
+		return user.getCredentials().getPassword();
+	}
+
+	@Override
+	public String getUsername()
+	{
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked()
+	{
+		return !user.isLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired()
+	{
+		return user.getCredentials().isNonExpired();
+	}
+
+	@Override
+	public boolean isEnabled()
+	{
+		return user.isEnabled();
+	}
+
+}
