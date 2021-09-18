409947c34df5ed62e6e5082d4d91efbbd9c03f40
Hadi <hadidanial97@gmail.com>
2021-09-18 13:31:41 +0300

    Update SpringSecurityApplication.java

--- Backend/src/main/java/hadi/springSecurity/SpringSecurityApplication.java
+++ Backend/src/main/java/hadi/springSecurity/SpringSecurityApplication.java
@@ -2,12 +2,15 @@ package hadi.springSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import hadi.springSecurity.BL.UserBL;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
 }
