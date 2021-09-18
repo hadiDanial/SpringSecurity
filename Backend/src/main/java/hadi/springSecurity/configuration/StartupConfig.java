ede70e15fdf0fd2612cec089e616e51919c44e4e
Hadi <hadidanial97@gmail.com>
2021-09-18 19:33:29 +0300

    Added two users on startup

--- Backend/src/main/java/hadi/springSecurity/configuration/StartupConfig.java
+++ Backend/src/main/java/hadi/springSecurity/configuration/StartupConfig.java
@@ -1,16 +1,37 @@
 package hadi.springSecurity.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
-public class StartupConfig implements ApplicationListener<ContextRefreshedEvent> 
+import hadi.springSecurity.BL.UserBL;
+
+@Configuration
+@Component
+public class StartupConfig implements ApplicationListener<ContextRefreshedEvent>
 {
+	private final UserBL userBL;
+
+	@Autowired
+	public StartupConfig(UserBL userBL)
+	{
+		super();
+		this.userBL = userBL;
+	}
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event)
 	{
-		// TODO Auto-generated method stub
-		
+		if (userBL.findUserByUsername("John") == null)
+		{		
+			userBL.createUser("John", "johnnyboy@gmail.com", "MagicalDaddy69", "John", null, "Constantine");
+		}
+		if (userBL.findUserByUsername("Bill") == null)
+		{		
+			userBL.createUser("Bill", "bill@gmail.com", "12345", "Bill", "The", "Drill");
+		}
 	}
 
 }
