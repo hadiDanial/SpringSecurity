c954740adda94fb9325bdac44b59278ce8077367
Hadi <hadidanial97@gmail.com>
2021-09-18 13:54:34 +0300

    Update UserBL.java

--- Backend/src/main/java/hadi/springSecurity/BL/UserBL.java
+++ Backend/src/main/java/hadi/springSecurity/BL/UserBL.java
@@ -1,21 +1,115 @@
 package hadi.springSecurity.BL;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import hadi.springSecurity.beans.embeddables.Credential;
+import hadi.springSecurity.beans.embeddables.Name;
+import hadi.springSecurity.beans.entities.User;
 import hadi.springSecurity.configuration.Properties;
+import hadi.springSecurity.repositories.UserRepository;
 
 @Service
 public class UserBL
 {
+	private final UserRepository userRepository;
 	private final Properties properties;
-
 	@Autowired
-	public UserBL(Properties properties)
+	public UserBL(UserRepository userRepository, Properties properties)
 	{
 		super();
 		this.properties = properties;
+		this.userRepository = userRepository;
 	}
 	
+	
+	public boolean createUser(String username, String email, String password, String firstName, String middleName, String lastName)
+	{
+		Name name = generateName(firstName, middleName, lastName);
+		Credential credentials = generateCredentials(password);
+		User user = new User(username, email, name, credentials);
+		try
+		{
+			userRepository.save(user);
+			return true;
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+
+	public boolean updatePassword(String username, String password)
+	{
+		User user = findUserByUsername(username);
+		if(user == null)
+			return false;
+		try
+		{
+			Credential credentials = generateCredentials(password);
+			user.setCredentials(credentials);
+			userRepository.save(user);
+			return true;	
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean updateName(String username, String firstName, String middleName, String lastName)
+	{
+		User user = findUserByUsername(username);
+		if(user == null)
+			return false;
+		try
+		{
+			Name name = generateName(firstName, middleName, lastName);
+			user.setName(name);
+			userRepository.save(user);
+			return true;	
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+
+	private User findUserByUsername(String username)
+	{
+		Optional<User> optionalUser = userRepository.findUserByUsername(username);
+		User user = optionalUser.orElse(null);
+		return user;
+	}
+
+
+	private Credential generateCredentials(String password)
+	{
+		String hashedPassword = password; // Use encoder
+		return new Credential(hashedPassword, LocalDateTime.now().plusDays(properties.getCredentialExpirationDays()));
+	}
+	
+
+
+	private Name generateName(String firstName, String middleName, String lastName)
+	{
+		Name name;
+		if(middleName == null || middleName.equals(""))
+		{
+			name = new Name(firstName, lastName);			
+		}
+		else
+		{			
+			name = new Name(firstName, middleName, lastName);
+		}
+		return name;
+	}
 	
 }
