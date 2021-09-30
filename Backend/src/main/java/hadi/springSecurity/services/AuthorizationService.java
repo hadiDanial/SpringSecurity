32a435bb5d1ad3e2795f3ac77fa2175fec97b130
Hadi <hadidanial97@gmail.com>
2021-10-01 01:09:06 +0300

    Update AuthorizationService.java
    Added logout, refresh, validate

--- Backend/src/main/java/hadi/springSecurity/services/AuthorizationService.java
+++ Backend/src/main/java/hadi/springSecurity/services/AuthorizationService.java
@@ -1,44 +1,73 @@
 package hadi.springSecurity.services;
 
+import java.time.Instant;
+
+import javax.el.MethodNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import hadi.springSecurity.configuration.Properties;
+import hadi.springSecurity.models.entities.Token;
 import hadi.springSecurity.models.entities.User;
 import hadi.springSecurity.models.requests.LoginRequest;
-import hadi.springSecurity.models.responses.AuthenticationResponse;
+import hadi.springSecurity.models.requests.ValidateTokenRequest;
+import hadi.springSecurity.models.responses.TokenResponse;
 import hadi.springSecurity.security.services.JwtProvider;
 
 @Service
 public class AuthorizationService
 {
 	private final UserService userService;
+	private final TokenService tokenService;
 	private final PasswordEncoder passwordEncoder;
 	private final AuthenticationManager authManager;
-	private final JwtProvider jwtProvider;
+
 	@Autowired
-	public AuthorizationService(JwtProvider jwtProvider, AuthenticationManager authManager, 
-								UserService userBL, PasswordEncoder passwordEncoder)
+	public AuthorizationService(AuthenticationManager authManager, 
+								UserService userService, TokenService tokenService, PasswordEncoder passwordEncoder)
 	{
 		super();
-		this.userService = userBL;
+		this.userService = userService;
 		this.passwordEncoder = passwordEncoder;
 		this.authManager = authManager;
-		this.jwtProvider = jwtProvider;
+		this.tokenService = tokenService;
 	}
 
-	public AuthenticationResponse login(LoginRequest loginRequest)
+	public Token login(LoginRequest loginRequest)
 	{
 //		authManager.authenticate(null)
 		User user = userService.findUserByUsername(loginRequest.getUsername());
-		if(user == null || 
-				passwordEncoder.matches(loginRequest.getPassword(), user.getCredentials().getPassword()))
+		if(user == null)
 		{
-			AuthenticationResponse response = jwtProvider.generateAuthResponse(user);
+			throw new BadCredentialsException("Login failed, " + loginRequest.getUsername() + " doesn't exist.");
+		}
+		if(passwordEncoder.matches(loginRequest.getPassword(), user.getCredentials().getPassword()))
+		{
+			Token response = tokenService.generateToken(user);
+			user.setLastAccessDate(Instant.now());
 			return response;
 		}
-		throw new BadCredentialsException("Login failed, bad credentials for " + loginRequest.getUsername());
+		throw new BadCredentialsException("Login failed, bad credentials for " + loginRequest.getUsername() + ".");
+	}
+	
+	public void logout(String refreshToken)
+	{
+		tokenService.deleteToken(refreshToken);
+	}
+
+	public String refreshAuthToken(String refreshToken)
+	{
+		// TODO Auto-generated method stub
+		throw new MethodNotFoundException();
+	}
+
+	public TokenResponse validate(ValidateTokenRequest validateTokenRequest)
+	{
+		//"Session expired, please log in again..." "Invalid credentials, log in again..."
+		return null;
 	}
 }
