fc454182dcf8cf9df4073a1bd534b8547c6ad89e
Hadi <hadidanial97@gmail.com>
2021-10-01 01:08:21 +0300

    Added TokenService and Repository

---
+++ Backend/src/main/java/hadi/springSecurity/repositories/TokenRepository.java
@@ -0,0 +1,15 @@
+package hadi.springSecurity.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hadi.springSecurity.models.entities.Token;
+
+public interface TokenRepository extends JpaRepository<Token, Long>
+{
+	Optional<Token> findByUsername(String username);
+
+	Optional<Token> findByRefreshToken(String refreshToken);
+	Optional<Token> findByUsernameAndRefreshToken(String username, String refreshToken);
+}
---
+++ Backend/src/main/java/hadi/springSecurity/services/TokenService.java
@@ -0,0 +1,98 @@
+package hadi.springSecurity.services;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hadi.springSecurity.configuration.Properties;
+import hadi.springSecurity.exceptions.TokenException;
+import hadi.springSecurity.models.entities.Token;
+import hadi.springSecurity.models.entities.User;
+import hadi.springSecurity.repositories.TokenRepository;
+import hadi.springSecurity.security.services.JwtProvider;
+
+@Service
+public class TokenService
+{
+	private final TokenRepository tokenRepository;
+	private final JwtProvider jwtProvider;
+	private final Properties properties;
+
+	@Autowired
+	public TokenService(JwtProvider jwtProvider, TokenRepository tokenRepository, Properties properties)
+	{
+		super();
+		this.tokenRepository = tokenRepository;
+		this.jwtProvider = jwtProvider;
+		this.properties = properties;
+	}
+
+	public void saveToken(Token token)
+	{
+		try
+		{
+			tokenRepository.save(token);
+
+		} catch (Exception e)
+		{
+			throw new TokenException("Token Save Exception");
+		}
+	}
+
+	public void deleteToken(Token token)
+	{
+		try
+		{
+			Token t = tokenRepository.findById(token.getId()).get();
+			tokenRepository.delete(t);
+		} catch (Exception e)
+		{
+			throw new TokenException("Token Delete Exception");
+		}
+	}
+
+	public Token generateToken(User user)
+	{
+		Token token = new Token();
+		Date tokenExpirationDate = jwtProvider.generateTokenExpirationDate(properties.getTokenLifetime());
+		Date refreshTokenExpirationDate = jwtProvider.generateTokenExpirationDate(properties.getRefreshTokenLifetime());
+		token.setAuthenticationToken(jwtProvider.generateToken(user.getUsername(), tokenExpirationDate));
+		token.setRefreshToken(jwtProvider.generateToken(user.getUsername(), refreshTokenExpirationDate));
+		token.setExpiresAt(tokenExpirationDate.toInstant());
+		token.setUsername(user.getUsername());
+		tokenRepository.save(token);
+		return token;
+	}
+
+	public Token generateNewAuthToken(String refreshToken)
+	{
+		Token token = findTokenByRefreshToken(refreshToken);
+		Date tokenExpirationDate = jwtProvider.generateTokenExpirationDate(properties.getTokenLifetime());
+		token.setAuthenticationToken(
+				jwtProvider.generateToken(jwtProvider.getUsernameFromToken(refreshToken), tokenExpirationDate));
+		tokenRepository.save(token);
+		return token;
+	}
+
+	private Token findTokenByRefreshToken(String refreshToken)
+	{
+		Token token = tokenRepository.findByRefreshToken(refreshToken)
+				.orElseThrow(() -> new TokenException("Token not found by refresh token"));
+		return token;
+	}
+
+	private Token findTokenByUsername(String username)
+	{
+		Token token = tokenRepository.findByUsername(username)
+				.orElseThrow(() -> new TokenException("Token not found by username"));
+		return token;
+	}
+
+	public void deleteToken(String refreshToken)
+	{
+		Token token = findTokenByRefreshToken(refreshToken);
+		tokenRepository.delete(token);
+	}
+
+}
