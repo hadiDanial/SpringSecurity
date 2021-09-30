50b92841839c8f984a1c7ab8e1b06824a50a0f20
Hadi <hadidanial97@gmail.com>
2021-10-01 00:27:01 +0300

    Update UserRepository.java

--- Backend/src/main/java/hadi/springSecurity/repositories/UserRepository.java
+++ Backend/src/main/java/hadi/springSecurity/repositories/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hadi.springSecurity.models.entities.User;
 
 
-public interface UserRepository  extends JpaRepository<User,Long>
+public interface UserRepository extends JpaRepository<User,Long>
 {
 	Optional<User> findUserByUsername(String username);
 	boolean existsByUsername(String username);
