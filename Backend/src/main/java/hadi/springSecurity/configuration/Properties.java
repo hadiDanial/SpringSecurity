c8968617687a962f10a5c873945cae18fa14f1e3
Hadi <hadidanial97@gmail.com>
2021-10-01 00:25:48 +0300

    Added Issuer to Properties

--- Backend/src/main/java/hadi/springSecurity/configuration/Properties.java
+++ Backend/src/main/java/hadi/springSecurity/configuration/Properties.java
@@ -1,15 +1,12 @@
 package hadi.springSecurity.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @PropertySource("classpath:application.properties")
 @Component
-public class Properties //implements ApplicationListener<ContextRefreshedEvent> 
+public class Properties
 {
 	@Value("${auth.credentials.expire-in-days}")
 	private int credentialExpirationDays;
@@ -18,7 +15,9 @@ public class Properties //implements ApplicationListener<ContextRefreshedEvent>
 	@Value("${auth.refresh-token-lifetime-mins}")
 	private int refreshTokenLifetime;
 	@Value("${jwt.secret}")
-	private String secret;
+	private String jwtSecret;
+	@Value("${jwt.issuer}")
+	private String jwtIssuer;
 	
 	public int getCredentialExpirationDays()
 	{
@@ -35,8 +34,15 @@ public class Properties //implements ApplicationListener<ContextRefreshedEvent>
 		return refreshTokenLifetime;
 	}
 
-	public String getSecret()
+	public String getJwtSecret()
 	{
-		return secret;
+		return jwtSecret;
 	}
+
+	public String getJwtIssuer()
+	{
+		return jwtIssuer;
+	}
+	
+	
 }
