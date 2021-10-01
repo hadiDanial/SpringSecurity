26a9dd753ace7386604073857d02f6c0bcae5536
Hadi <hadidanial97@gmail.com>
2021-10-01 20:08:11 +0300

    Added methods to Role, Authority repositories
    Added findBy-Identifier, existsBy-Identifier methods

--- Backend/src/main/java/hadi/springSecurity/repositories/AuthorityRepository.java
+++ Backend/src/main/java/hadi/springSecurity/repositories/AuthorityRepository.java
@@ -1,10 +1,13 @@
 package hadi.springSecurity.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hadi.springSecurity.models.entities.Authority;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long>
 {
-
+	Optional<Authority> findByAuthorityIdentifier(String authorityIdentifier);
+	boolean existsByAuthorityIdentifier(String authorityIdentifier);
 }
--- Backend/src/main/java/hadi/springSecurity/repositories/RoleRepository.java
+++ Backend/src/main/java/hadi/springSecurity/repositories/RoleRepository.java
@@ -1,10 +1,13 @@
 package hadi.springSecurity.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hadi.springSecurity.models.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long>
 {
-
+	Optional<Role> findByRoleIdentifier(String roleIdentifier);
+	boolean existsByRoleIdentifier(String roleIdentifier);
 }
