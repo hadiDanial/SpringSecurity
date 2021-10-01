2bb94d39b5b2617965593d81660bef4791b9c129
Hadi <hadidanial97@gmail.com>
2021-10-01 20:01:48 +0300

    Update StartupConfig.java

--- Backend/src/main/java/hadi/springSecurity/configuration/StartupConfig.java
+++ Backend/src/main/java/hadi/springSecurity/configuration/StartupConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import hadi.springSecurity.services.UserService;
@@ -24,13 +25,25 @@ public class StartupConfig implements ApplicationListener<ContextRefreshedEvent>
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event)
 	{
-		if (userService.findUserByUsername("John") == null)
-		{		
-			userService.createUser("John", "johnnyboy@gmail.com", "MagicalDaddy69", "John", null, "Constantine");
+		generateDummyUsers();
+		
+	}
+
+	private void generateDummyUsers()
+	{
+		try
+		{
+			userService.findUserByUsername("John");
+		} catch (UsernameNotFoundException e)
+		{
+			userService.createNewUser("John", "johnnyboy@gmail.com", "MagicalDaddy69", "John", null, "Constantine");
 		}
-		if (userService.findUserByUsername("Bill") == null)
-		{		
-			userService.createUser("Bill", "bill@gmail.com", "12345", "Bill", "The", "Drill");
+		try
+		{
+			userService.findUserByUsername("Bill");
+		} catch (UsernameNotFoundException e)
+		{
+			userService.createNewUser("Bill", "bill@gmail.com", "12345", "Bill", "The", "Drill");
 		}
 	}
 
