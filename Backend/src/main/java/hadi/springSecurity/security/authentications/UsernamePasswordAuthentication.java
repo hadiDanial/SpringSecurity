8aa41e1d9fbb38a7617ca1dede38cceb0444051a
Hadi <hadidanial97@gmail.com>
2021-10-03 22:55:44 +0300

    Update UsernamePasswordAuthentication.java

--- Backend/src/main/java/hadi/springSecurity/security/authentications/UsernamePasswordAuthentication.java
+++ Backend/src/main/java/hadi/springSecurity/security/authentications/UsernamePasswordAuthentication.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 public class UsernamePasswordAuthentication extends UsernamePasswordAuthenticationToken
 {
+	private static final long serialVersionUID = -4363412539437295590L;
 
 	public UsernamePasswordAuthentication(Object principal, Object credentials)
 	{
