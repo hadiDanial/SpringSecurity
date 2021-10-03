b1c57ad192f68a90a2ab642df66509bfbd44cebf
Hadi <hadidanial97@gmail.com>
2021-10-03 22:55:25 +0300

    Update WebSecurityConfiguration.java

--- Backend/src/main/java/hadi/springSecurity/configuration/WebSecurityConfiguration.java
+++ Backend/src/main/java/hadi/springSecurity/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,12 +15,18 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import hadi.springSecurity.security.filters.UsernamePasswordAuthFilter;
+import hadi.springSecurity.security.providers.UsernamePasswordAuthProvider;
+import hadi.springSecurity.services.AuthenticationService;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
-//	private final UsernamePasswordAuthenticationProvider usernamePasswordAuthProvider;
-//	private final UsernamePasswordAuthenticationFilter usernamePasswordAuthFilter;
+	private final UsernamePasswordAuthProvider usernamePasswordAuthProvider;
+	private final UsernamePasswordAuthFilter usernamePasswordAuthFilter;
+	private final AuthenticationService authenticationService;
+	
 	private static final String[] AUTH_WHITELIST = {
             // -- Swagger UI v2
             "/v2/api-docs",
@@ -36,24 +43,27 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
             "/auth/"
     };
 	
-//	@Autowired
-//	public WebSecurityConfiguration(UsernamePasswordAuthenticationProvider usernamePasswordAuthProvider,
-//			UsernamePasswordAuthenticationFilter usernamePasswordAuthFilter)
-//	{
-//		this.usernamePasswordAuthProvider = usernamePasswordAuthProvider;
-//		this.usernamePasswordAuthFilter = usernamePasswordAuthFilter;
-//	}
-//
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception
-//	{
-//		auth.authenticationProvider(usernamePasswordAuthProvider);
-//	}
+	@Autowired
+	@Lazy
+	public WebSecurityConfiguration(UsernamePasswordAuthProvider usernamePasswordAuthProvider,
+			UsernamePasswordAuthFilter usernamePasswordAuthFilter,
+			AuthenticationService authenticationService)
+	{
+		this.usernamePasswordAuthProvider = usernamePasswordAuthProvider;
+		this.usernamePasswordAuthFilter = usernamePasswordAuthFilter;
+		this.authenticationService = authenticationService;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	{
+		auth.authenticationProvider(usernamePasswordAuthProvider);
+	}
 //
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-//		http.addFilterAt(usernamePasswordAuthFilter, BasicAuthenticationFilter.class);
+		http.addFilterAt(usernamePasswordAuthFilter, BasicAuthenticationFilter.class);
 		http.csrf().disable(); // lesson 9 // focus on CORS
 //		http.cors();
 		http.httpBasic();
@@ -83,4 +93,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 		return super.authenticationManagerBean();
 	}
 
+ 	@Bean
+ 	public UsernamePasswordAuthProvider usernamePasswordAuthProvider()
+ 	{
+ 		return new UsernamePasswordAuthProvider(authenticationService);
+ 	}
+ 	@Bean
+ 	public UsernamePasswordAuthFilter usernamePasswordAuthFilter() throws Exception
+ 	{
+ 		return new UsernamePasswordAuthFilter(authenticationManager());
+ 	}
 }
