3087d5416d88a2765a064c0fb6be77f947f1dc9b
Hadi <hadidanial97@gmail.com>
2021-10-06 01:32:21 +0300

    Added the ability to add and remove roles from a user

--- Backend/src/main/java/hadi/springSecurity/services/UserService.java
+++ Backend/src/main/java/hadi/springSecurity/services/UserService.java
@@ -13,9 +13,12 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import hadi.springSecurity.configuration.Properties;
+import hadi.springSecurity.exceptions.RoleException;
 import hadi.springSecurity.models.embeddables.Credential;
 import hadi.springSecurity.models.embeddables.Name;
+import hadi.springSecurity.models.entities.Role;
 import hadi.springSecurity.models.entities.User;
+import hadi.springSecurity.models.responses.MessageBoolResponse;
 import hadi.springSecurity.models.security.SecurityUser;
 import hadi.springSecurity.repositories.UserRepository;
 
@@ -25,14 +28,16 @@ public class UserService implements UserDetailsManager
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final Properties properties;
+	private final RoleService roleService;
 
 	@Autowired
-	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, Properties properties)
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, Properties properties, RoleService roleService)
 	{
 		super();
 		this.properties = properties;
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.roleService = roleService;
 	}
 
 	public User createNewUser(String username, String email, String password, String firstName, String middleName,
@@ -94,7 +99,12 @@ public class UserService implements UserDetailsManager
 		}
 	}
 
-	public User findUserByUsername(String username)
+	/**
+	 * Returns user by username. Throws <code>UsernameNotFoundException</code> if the username isn't found.
+	 * @param username
+	 * @return
+	 */
+	public User findUserByUsername(String username) throws UsernameNotFoundException
 	{
 		Optional<User> optionalUser = userRepository.findUserByUsername(username);
 		User user = optionalUser.orElseThrow(()-> new UsernameNotFoundException(username + " not found."));
@@ -168,4 +178,45 @@ public class UserService implements UserDetailsManager
 	{
 	}
 
+	public MessageBoolResponse addRoleToUser(User user, String roleName)
+	{
+		Role role;
+		try
+		{
+			role = roleService.findRoleByName(roleName);			
+		} catch (RoleException e)
+		{
+			return new MessageBoolResponse("Role " + roleName + " doesn't exist.", false);
+		}
+		List<Role> userRoles = user.getRoles();
+		if(userRoles.contains(role))
+		{
+			return new MessageBoolResponse("User " + user.getUsername() +" already has role " + roleName + ".", false);
+		}
+		userRoles.add(role);
+		user.setRoles(userRoles);
+		userRepository.save(user);
+		return new MessageBoolResponse("Role " + roleName + " added to " + user.getUsername() + " successfully.", true);
+	}
+	
+	public MessageBoolResponse removeRoleFromUser(User user, String roleName)
+	{
+		Role role;
+		try
+		{
+			role = roleService.findRoleByName(roleName);			
+		} catch (RoleException e)
+		{
+			return new MessageBoolResponse("Role " + roleName + " doesn't exist.", false);
+		}
+		List<Role> userRoles = user.getRoles();
+		if(userRoles.contains(role))
+		{
+			userRoles.remove(role);
+			user.setRoles(userRoles);
+			userRepository.save(user);
+			return new MessageBoolResponse("Role " + roleName + " removed from " + user.getUsername() + " successfully.", true);
+		}
+		return new MessageBoolResponse("User " + user.getUsername() +" doesn't have role " + roleName + ".", false);
+	}
 }
