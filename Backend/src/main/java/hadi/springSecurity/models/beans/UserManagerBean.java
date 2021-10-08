12481f21528e341ca9d35d722e6ea456aac6fc1b
Hadi <hadidanial97@gmail.com>
2021-10-08 15:52:47 +0300

    Update UserManagerBean.java

--- Backend/src/main/java/hadi/springSecurity/models/beans/UserManagerBean.java
+++ Backend/src/main/java/hadi/springSecurity/models/beans/UserManagerBean.java
@@ -3,29 +3,26 @@ package hadi.springSecurity.models.beans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import hadi.springSecurity.services.UserService;
-import managers.SecurityUserManager;
 
 @Configuration
 public class UserManagerBean
 {
 	private final UserService userService;
-	private final PasswordEncoder passwordEncoder;
 	
 	@Autowired
-	public UserManagerBean(UserService userService, PasswordEncoder passwordEncoder)
+	public UserManagerBean(UserService userService)
 	{
 		super();
 		this.userService = userService;
-		this.passwordEncoder = passwordEncoder;
 	}
 
 
 	@Bean
-	public SecurityUserManager securityUserManager()
+	public UserDetailsService securityUserManager()
 	{
-		return new SecurityUserManager(userService, passwordEncoder);
+		return userService;
 	}
 }
