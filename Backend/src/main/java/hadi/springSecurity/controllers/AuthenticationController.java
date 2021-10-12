74481a0e43e3639bdae73cdf28b5a5458d758052
Hadi <hadidanial97@gmail.com>
2021-10-12 23:12:03 +0300

    Added CrossOrigin to AuthenticationController

--- Backend/src/main/java/hadi/springSecurity/controllers/AuthenticationController.java
+++ Backend/src/main/java/hadi/springSecurity/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,6 +22,7 @@ import hadi.springSecurity.services.AuthenticationService;
 
 @RestController
 @RequestMapping(path = "/auth")
+@CrossOrigin
 public class AuthenticationController
 {
 	private final AuthenticationService authService;
