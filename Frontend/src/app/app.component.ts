2cda1a13c8d826eabd78c9716ccad62958c16484
Hadi <hadidanial97@gmail.com>
2022-02-12 01:48:52 +0200

    App.Component now gets the logged in user details by using the token, authService now supports logout

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from './models/entities/User';
+import { AuthService } from './services/authService/auth.service';
+import { UserService } from './services/userService/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+  userSubscription: Subscription = new Subscription();
+  currentUser : User = this.userService.user;
+  constructor(private userService: UserService, private authService: AuthService){}
+  ngOnInit(): void
+  {
+    setTimeout(()=> {
+      let token = this.authService.getToken();
+      if(token)
+      {
+        this.authService.getUserByToken();
+      }
+  }, 500);
+    this.userSubscription = this.userService.currentUser.subscribe(currentUser => 
+      {
+        this.currentUser = currentUser;
+      });
+  }
+  ngOnDestroy(): void
+  {
+    this.userSubscription.unsubscribe();
+  }
+
   title = 'Frontend';
 }
--- Frontend/src/app/services/authService/auth.service.ts
+++ Frontend/src/app/services/authService/auth.service.ts
@@ -1,30 +1,121 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/entities/User';
+import { Name } from 'src/app/models/other/Name';
 import { LoginRequest } from 'src/app/models/requests/LoginRequest';
 import { LoginResponse } from 'src/app/models/responses/LoginResponse';
+import { UserService } from '../userService/user.service';
 import { WebService } from '../webService/web.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+
+
+  getUserByToken()
+  {
+    this.webService.get<User>(this.PATH_URL+"/getUserByToken").subscribe(user=>{
+      let newUser = this.dataToUser(user);
+      this.userService.changeUser(newUser);
+    }, error=>
+    {
+      alert("Failed to get user by token, clearing storage");
+      this.removeTokenDataFromStorage();
+    });
+  }
+
   readonly PATH_URL = "auth";
-  constructor(private webService: WebService) { }
+  constructor(private webService: WebService, private userService: UserService) { }
   loginResponse : LoginResponse | undefined;
   
   public login(loginRequest: LoginRequest)
   {
     // if(this.loginResponse != undefined)
     //   return;
-    localStorage.clear();
+    localStorage.removeItem("token");
     let map = new Map<string, any>();
-    map.set("loginRequest", loginRequest);
-    this.webService.post<LoginResponse>(this.PATH_URL + "/login", map).subscribe(response=>
+    let message: string = "";
+    map.set("username", loginRequest.username);
+    map.set("password", loginRequest.password);
+    let obs = this.webService.post<LoginResponse>(this.PATH_URL + "/login",undefined, map);
+    obs.subscribe(response=>
       {
-        console.log("Login message: " + response.$message);
+        console.log("Login message: " + response.message);
         this.loginResponse = response;
+        message = response.message;
+        console.log(message);
+        let user = response.user;
+        let newUser = this.dataToUser(user);
+        this.userService.changeUser(newUser);
         localStorage.setItem("token",response.token.accessToken);
+        localStorage.setItem("refreshToken", response.token.refreshToken);
+        localStorage.setItem("tokenExpiration", response.token.expiresAt+'');
+      },
+      error=>
+      {
+        let httpError : (HttpErrorResponse)  = error;
+        let headers = httpError.headers;
+        console.log("Status: " + httpError.status);
+        this.userService.changeUser(User.getDefaultUser());
+        // TODO: Alert, add generic error handler
+        alert("Failed to login!");
       });
     }
 
+
+  public logout()
+  {
+    this.webService.post<void>(this.PATH_URL+"/logout").subscribe(()=>{
+      this.removeTokenDataFromStorage();
+      
+    }, error=>
+    {
+      //this.removeTokenDataFromStorage();
+      alert("Failed to log out!");
+    });
+  }
+  
+  private removeTokenDataFromStorage()
+  {
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("tokenExpiration");
+    this.userService.changeUser(User.getDefaultUser());
+  }
+
+  private dataToUser(user: User)
+  {
+    return new User(user.id, user.username, user.email, new Name(user.name.firstName, user.name.lastName, user.name.middleName));
+  }
+
+    /**
+     * @returns returns the access token if it exists and hasn't expired yet. Deletes it if it has expired.
+     */
+    public getToken()
+    {
+      let token = localStorage.getItem("token");
+      if(token)
+      {
+        // TODO: Need to check if token has expired, and if so, acquire a new token from the server (as long as the refresh token is valid)
+        let expiresAt :Date = new Date(localStorage.getItem("tokenExpiration")+"");
+        if(expiresAt > new Date())
+        {
+          return token;
+        }
+        else
+        {
+          localStorage.removeItem("token");
+          localStorage.removeItem("tokenExpiration");
+        }
+      }
+      return null;
+    }
+
+    getRefreshToken()
+    {
+      let refreshToken = localStorage.getItem("refreshToken");
+      // TODO: Need to check if token has expired, and if so, delete and force the user to relog
+      return refreshToken;
+    }
   }
