afd16801af5d0260fc14b8ba21315585678cb25b
Hadi <hadidanial97@gmail.com>
2022-02-12 01:48:04 +0200

    Added expiresAt to Token model (frontend)

--- Frontend/src/app/models/entities/Token.ts
+++ Frontend/src/app/models/entities/Token.ts
@@ -2,11 +2,12 @@ export class Token
 {
     private _accessToken: string;
     private _refreshToken: string;
-
-    constructor(accessToken: string, refreshToken: string)
+    private _expiresAt: Date;
+    constructor(accessToken: string, refreshToken: string, expiresAt:Date)
     {
         this._accessToken = accessToken;
         this._refreshToken = refreshToken;
+        this._expiresAt = expiresAt;
     }
 
     public get accessToken(): string
@@ -17,6 +18,11 @@ export class Token
     public get refreshToken(): string
     {
         return this._refreshToken;
+    }
+
+    public get expiresAt(): Date
+    {
+        return this._expiresAt;
     }
 
     public set accessToken(accessToken: string)
