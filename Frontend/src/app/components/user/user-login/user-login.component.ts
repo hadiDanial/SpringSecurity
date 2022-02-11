d2f59956246ff0237c1e04bea164cf2f97e00c78
Hadi <hadidanial97@gmail.com>
2022-02-11 21:58:25 +0200

    Functional login page
    Terrible design though :smile:
    Need to see how angular forms work and implement those here

--- Frontend/src/app/components/user/user-login/user-login.component.ts
+++ Frontend/src/app/components/user/user-login/user-login.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/entities/User';
+import { LoginRequest } from 'src/app/models/requests/LoginRequest';
+import { AuthService } from 'src/app/services/authService/auth.service';
 import { UserService } from 'src/app/services/userService/user.service';
 
 @Component({
@@ -10,10 +15,50 @@ export class UserLoginComponent implements OnInit
 {
   password:string="";
   username:string="";
-  constructor(private userService: UserService) { }
+  isLoading = false;
+  isLoggedIn = false;
+  triedToLogin = false;
+  defaultMessage = "Please enter your username and password to sign in.";
+  successfulLoginMessage = "Signed in successfully. Welcome back, ";
+  failedLoginMessage = "Failed to sign in. Please try again.";
+  currentMessage = "";
+  userSubscription: Subscription = new Subscription();
+  currentUser : User = this.userService.user;
+  constructor(private userService: UserService, private authService:AuthService, private router: Router) { }
 
   ngOnInit(): void
   {
+    this.currentMessage = this.defaultMessage;
+    this.userSubscription = this.userService.currentUser.subscribe(currentUser => 
+      {
+        this.currentUser = currentUser;
+        let equals = this.currentUser.equals(User.getDefaultUser());
+        if(!this.isLoggedIn && this.triedToLogin)
+        {
+          if(equals) // Error
+          {               
+            this.currentMessage = this.failedLoginMessage;        
+          }
+          else
+          {
+            this.currentMessage = this.successfulLoginMessage + this.currentUser.getName() + "!";
+            this.isLoggedIn = true;
+            this.password="";
+            this.router.navigate(['/home']);
+            // TODO: Redirect to home
+          }
+
+        }
+        else
+        {
+          if(!equals)
+          {
+            console.log("Not equals");
+            //this.isLoggedIn = true;
+          }
+        }
+        console.log("Is logged in: " + this.isLoggedIn);
+      })
   }
 
   updateUsername(username: any)  
@@ -28,7 +73,11 @@ export class UserLoginComponent implements OnInit
   login()
   {
     console.log(this.username + " " + this.password);
-    this.userService.login(this.username, this.password);
+    let req = new LoginRequest(this.username, this.password);
+    this.triedToLogin = true;
+    this.authService.login(req);
+
+
   }
   getAllUsers()
   {
