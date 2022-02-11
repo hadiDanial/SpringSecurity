11259134fafbc508485393bbc87da77e620a3a8f
Hadi <hadidanial97@gmail.com>
2022-02-11 23:59:01 +0200

    Added global user and a BehaviorSubject for it
    The UserService now notifies everyone whenever the current user changes, and saves a copy of the current user for easy access.

--- Frontend/src/app/services/userService/user.service.ts
+++ Frontend/src/app/services/userService/user.service.ts
@@ -5,19 +5,24 @@ import { LoginResponse } from 'src/app/models/responses/LoginResponse';
 import { LoginRequest } from 'src/app/models/requests/LoginRequest';
 import { User } from 'src/app/models/entities/User';
 import { AuthService } from '../authService/auth.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private webService: WebService, private authService: AuthService) { }
+  constructor(private webService: WebService) { }
+  user : User = User.getDefaultUser();
+  private userSource = new BehaviorSubject<User>(this.user);
+  currentUser = this.userSource.asObservable();
 
-  login(username:string, password:string)
-  {    
-    let req = new LoginRequest(username, password);
-    this.authService.login(req);
+  changeUser(user:User)
+  {
+    this.user = user;
+    this.userSource.next(user);
   }
+
   getAllUsers()
   {
     this.webService.get<User[]>("user/getAllUsers", new Map<string, any>()).subscribe((users)=>{console.log(users)})
