948df1628d3d168e4f3e4d29727a89e4a5fe82fd
Hadi <hadidanial97@gmail.com>
2022-02-11 21:54:17 +0200

    Added id to User, default user, getName, and equals

--- Frontend/src/app/models/entities/User.ts
+++ Frontend/src/app/models/entities/User.ts
@@ -1,44 +1,49 @@
-import { first } from "rxjs/operators";
 import { Name } from "../other/Name";
 
 export class User
 {
+    private static defaultUser = new User(0, "Guest", "",new Name("Guest",""));
+    private _id: number;
     private _username: string;
     private _email: string;
     private _name: Name;
-
-    constructor(username: string, email: string, name: Name) 
+    
+    constructor(id: number, username: string, email: string, name: Name) 
     {
+        this._id = id;
         this._username = username;
         this._email = email;
         this._name = name;
     }
-
+    public get id(): number
+    {
+        return this._id;
+    }
     public get username(): string
     {
         return this._username;
     }
-
+    
     public get email(): string
     {
         return this._email;
     }
-
+    
     public get name() : Name
     {
         return this._name;
     }
-
+    
     public set username(username:string)
     {
         this._username = username;
     }
-
+    
     public set email(email: string)
     {
         this._email = email;
     }
-
+    
     public set name(name: Name)
     {
         this._name = name;
@@ -48,5 +53,18 @@ export class User
     {
         let name: Name = new Name(firstName,lastName, middleName);
         this._name = name;
+    }
+    public getName():string
+    {
+        return this.name.toString();
+    }
+    static getDefaultUser(): User
+    {
+      return this.defaultUser;
+    }
+
+    public equals(other: User)
+    {
+      return this._username == other._username;
     }
 }
