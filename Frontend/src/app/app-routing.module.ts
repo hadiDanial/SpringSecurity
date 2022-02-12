800dd8c8848cf049c6a6871e3d3e88108b26827a
Hadi <hadidanial97@gmail.com>
2022-02-13 01:53:05 +0200

    Added signup page to routing/navigation

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { UserSignupComponent } from './components/user/user-signup/user-signup.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-   // outlet: "mainRouter"
   },
   {
     path: 'login',
     component: UserLoginComponent,
-   // outlet: "mainRouter"
+  },
+  {
+    path: 'signup',
+    component: UserSignupComponent,
   },
   {
     path: '',
