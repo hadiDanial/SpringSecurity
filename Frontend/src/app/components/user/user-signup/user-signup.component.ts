f81de7b53cc8ea49e2cd5ee3a2d2eedf68c7cf8d
Hadi <hadidanial97@gmail.com>
2022-02-16 21:35:29 +0200

    Added validation to User Sign Up Component form

--- Frontend/src/app/components/user/user-signup/user-signup.component.ts
+++ Frontend/src/app/components/user/user-signup/user-signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-user-signup',
   templateUrl: './user-signup.component.html',
@@ -7,11 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
   firstName = "";
   lastName = "";
   email = "";
+  minInputLen : number = 2;
+  maxInputLen : number = 30;
+  minPassLen : number = 2;
+  maxPassLen : number = 30;
+  allowedPattern: string = '[a-zA-Z ]*';
+  passwordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(.+?[#?!@$%^&*-]*).{'+this.minPassLen+','+this.maxPassLen+'}$';
+  form = new FormGroup({});
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        firstName: ["", [Validators.required, Validators.pattern(this.allowedPattern),Validators.minLength(this.minInputLen), Validators.maxLength(this.maxInputLen)]],
+        lastName: ["", [Validators.required, Validators.pattern(this.allowedPattern),Validators.minLength(this.minInputLen), Validators.maxLength(this.maxInputLen)]],
+        email: ["", [Validators.required, Validators.email]],
+        doublePasswordGroup: this.formBuilder.group(
+          {
+            password:["",[Validators.required, Validators.pattern(this.passwordPattern),Validators.minLength(this.minPassLen), Validators.maxLength(this.maxPassLen)]],
+            confirmPassword:["",[Validators.required, Validators.pattern(this.passwordPattern),Validators.minLength(this.minPassLen), Validators.maxLength(this.maxPassLen)]]
+          }
+        )
+      }
+    )
   }
-
+  onSubmit()
+  {
+    if(this.form.valid)
+    {
+      console.log("SUBMIT");
+      let firstName = this.form.value["firstName"];
+      let lastName = this.form.value["lastName"];
+      let email = this.form.value["email"];
+      let password = this.getGroupControl("doublePasswordGroup").value["password"];
+      console.log(firstName + " " + lastName + " - " + email + ", " + password);
+    }
+  }
+  getFormControl(controlName: string) : FormControl
+  {
+    return this.form.get(controlName) as FormControl;
+  }
+  getGroupControl(controlName: string) : FormGroup
+  {
+    return this.form.get(controlName) as FormGroup;
+  }
 }
