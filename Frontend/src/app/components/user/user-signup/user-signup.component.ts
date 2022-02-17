c7418800367fc021d00d542dff32592f19a6e9aa
Hadi <hadidanial97@gmail.com>
2022-02-17 02:19:00 +0200

    Implemented fully working user signup form

--- Frontend/src/app/components/user/user-signup/user-signup.component.ts
+++ Frontend/src/app/components/user/user-signup/user-signup.component.ts
@@ -1,33 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userService/user.service';
+import { WebService } from 'src/app/services/webService/web.service';
+import { ExistingValueValidator } from '../../shared/inputs/forms/checkExistingAsyncValidator';
 @Component({
   selector: 'app-user-signup',
   templateUrl: './user-signup.component.html',
   styleUrls: ['./user-signup.component.scss']
 })
-export class UserSignupComponent implements OnInit {
+export class UserSignupComponent implements OnInit
+{
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private webService: WebService) { }
   firstName = "";
   lastName = "";
   email = "";
-  minInputLen : number = 2;
-  maxInputLen : number = 30;
-  minPassLen : number = 2;
-  maxPassLen : number = 30;
-  allowedPattern: string = '[a-zA-Z ]*';
-  passwordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(.+?[#?!@$%^&*-]*).{'+this.minPassLen+','+this.maxPassLen+'}$';
+  minInputLen: number = 2;
+  maxInputLen: number = 30;
+  minUsernameLen: number = 3;
+  maxUsernameLen: number = 20;
+  minPassLen: number = 2;
+  maxPassLen: number = 30;
+  allowedNamePattern: string = '[a-zA-Z ]*';
+  disallowedNameMessage:string = 'Name must only contain letters.'
+  allowedUsernamePattern: string = '^[a-zA-Z][a-zA-Z0-9]*$';
+  disallowedUsernameMessage:string = 'Username must start with a letter and only contain letters and numbers.'
+  passwordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(.+?[#?!@$%^&*-]*).{' + this.minPassLen + ',' + this.maxPassLen + '}$';
   form = new FormGroup({});
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.form = this.formBuilder.group(
       {
-        firstName: ["", [Validators.required, Validators.pattern(this.allowedPattern),Validators.minLength(this.minInputLen), Validators.maxLength(this.maxInputLen)]],
-        lastName: ["", [Validators.required, Validators.pattern(this.allowedPattern),Validators.minLength(this.minInputLen), Validators.maxLength(this.maxInputLen)]],
+        firstName: ["", [Validators.required, Validators.pattern(this.allowedNamePattern), Validators.minLength(this.minInputLen), Validators.maxLength(this.maxInputLen)]],
+        lastName: ["", [Validators.required, Validators.pattern(this.allowedNamePattern), Validators.minLength(this.minInputLen), Validators.maxLength(this.maxInputLen)]],
+        username: ["", {validators:[Validators.required, Validators.pattern(this.allowedUsernamePattern), Validators.minLength(this.minUsernameLen), Validators.maxLength(this.maxUsernameLen)],
+                       asyncValidators:[ExistingValueValidator.checkExistingAsyncValidator("user/register/exists", "username", this.webService)], updateOn:'blur'}],
         email: ["", [Validators.required, Validators.email]],
         doublePasswordGroup: this.formBuilder.group(
           {
-            password:["",[Validators.required, Validators.pattern(this.passwordPattern),Validators.minLength(this.minPassLen), Validators.maxLength(this.maxPassLen)]],
-            confirmPassword:["",[Validators.required, Validators.pattern(this.passwordPattern),Validators.minLength(this.minPassLen), Validators.maxLength(this.maxPassLen)]]
+            password: ["", [Validators.required, Validators.pattern(this.passwordPattern), Validators.minLength(this.minPassLen), Validators.maxLength(this.maxPassLen)]],
+            confirmPassword: ["", [Validators.required, Validators.pattern(this.passwordPattern), Validators.minLength(this.minPassLen), Validators.maxLength(this.maxPassLen)]]
           }
         )
       }
@@ -35,21 +47,22 @@ export class UserSignupComponent implements OnInit {
   }
   onSubmit()
   {
-    if(this.form.valid)
+    if (this.form.valid)
     {
       console.log("SUBMIT");
       let firstName = this.form.value["firstName"];
       let lastName = this.form.value["lastName"];
+      let username = this.form.value["username"];
       let email = this.form.value["email"];
       let password = this.getGroupControl("doublePasswordGroup").value["password"];
-      console.log(firstName + " " + lastName + " - " + email + ", " + password);
+      this.userService.register(firstName, lastName, username, email, password);
     }
   }
-  getFormControl(controlName: string) : FormControl
+  getFormControl(controlName: string): FormControl
   {
     return this.form.get(controlName) as FormControl;
   }
-  getGroupControl(controlName: string) : FormGroup
+  getGroupControl(controlName: string): FormGroup
   {
     return this.form.get(controlName) as FormGroup;
   }
