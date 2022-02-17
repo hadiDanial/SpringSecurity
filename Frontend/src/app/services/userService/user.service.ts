68a51e67ea83b3542ee89236c7b47be16cb42504
Hadi <hadidanial97@gmail.com>
2022-02-17 02:20:01 +0200

    Added register function to userService

--- Frontend/src/app/services/userService/user.service.ts
+++ Frontend/src/app/services/userService/user.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { WebService } from './../webService/web.service';
-import { LoginResponse } from 'src/app/models/responses/LoginResponse';
-import { LoginRequest } from 'src/app/models/requests/LoginRequest';
 import { User } from 'src/app/models/entities/User';
-import { AuthService } from '../authService/auth.service';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { MessageBoolResponse } from 'src/app/models/responses/MessageBoolResponse';
+import { Name } from 'src/app/models/other/Name';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService
+{
 
   constructor(private webService: WebService) { }
-  user : User = User.getDefaultUser();
+  user: User = User.getDefaultUser();
   private userSource = new BehaviorSubject<User>(this.user);
   currentUser = this.userSource.asObservable();
 
-  changeUser(user:User)
+  changeUser(user: User)
   {
     this.user = user;
     this.userSource.next(user);
@@ -25,7 +24,21 @@ export class UserService {
 
   getAllUsers()
   {
-    this.webService.get<User[]>("user/getAllUsers", new Map<string, any>()).subscribe((users)=>{console.log(users)})
+    this.webService.get<User[]>("user/getAllUsers", new Map<string, any>()).subscribe((users) => { console.log(users) })
+  }
+
+  register(firstName: string, lastName: string, username: string, email: string, password: string)
+  {
+    let map = new Map<string, any>();
+    map.set("username", username);
+    map.set("password", password);
+    map.set("email", email);
+    map.set("firstName", firstName);
+    map.set("lastName", lastName);
+    this.webService.post<MessageBoolResponse>("user/register", map).subscribe(response =>
+    {
+      console.log(response.result + ": " + response.message);
+    })
   }
 
 }
