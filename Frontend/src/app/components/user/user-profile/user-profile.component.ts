04bb6b2c8f92e95ec98e22177088d9756e420315
Hadi <hadidanial97@gmail.com>
2022-02-22 00:35:54 +0200

    User Profile displays profile pic, name and username, allows uploading new pic

--- Frontend/src/app/components/user/user-profile/user-profile.component.ts
+++ Frontend/src/app/components/user/user-profile/user-profile.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/entities/User';
+import { FileService } from 'src/app/services/fileService/file.service';
+import { UserService } from 'src/app/services/userService/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit
+{
+  user: User = this.userService.user;
+  constructor(private userService: UserService, private fileService: FileService) { }
+  profileHandler = this.fileService.profileImageHandler;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    this.userService.currentUser.subscribe(user =>
+    {
+      this.user = user;
+    })
   }
+  uploadPicture()
+  {
 
+  }
 }
