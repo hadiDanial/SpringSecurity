d67d82c715bccaf702832cb4a3b0dbffcb06266a
Hadi <hadidanial97@gmail.com>
2022-02-21 10:42:39 +0200

    Upload/download profile image

--- Backend/src/main/java/hadi/springSecurity/services/ImageService.java
+++ Backend/src/main/java/hadi/springSecurity/services/ImageService.java
@@ -9,8 +9,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import hadi.springSecurity.models.entities.DBImage;
+import hadi.springSecurity.models.entities.ProfileImage;
+import hadi.springSecurity.models.entities.User;
+import hadi.springSecurity.models.entities.UserProfile;
 import hadi.springSecurity.repositories.DBImageRespository;
 import hadi.springSecurity.repositories.ProfileImageRepository;
+import hadi.springSecurity.repositories.UserRepository;
 
 @Component
 public class ImageService
@@ -18,12 +22,13 @@ public class ImageService
 	@Qualifier(value = "DBImageRespository")
 	private final DBImageRespository dbImageRepository;
 	private final ProfileImageRepository profileImageRepository;
-
-	public ImageService(DBImageRespository dbImageRepository, ProfileImageRepository profileImageRepository)
+	private final UserRepository userRepository;
+	public ImageService(DBImageRespository dbImageRepository, ProfileImageRepository profileImageRepository, UserRepository userRepository)
 	{
 		super();
 		this.dbImageRepository = dbImageRepository;
 		this.profileImageRepository = profileImageRepository;
+		this.userRepository = userRepository;
 	}
 
 	public Long uploadImage(MultipartFile multipartImage)
@@ -46,6 +51,39 @@ public class ImageService
 		if (image.isPresent())
 		{
 			byte[] imageBytes = image.get().getContent();
+			return new ByteArrayResource(imageBytes);
+		}
+		return null;
+	}
+
+	public Long uploadProfileImage(MultipartFile multipartImage, Long userId)
+	{
+		try
+		{
+			User user = userRepository.findById(userId).get();
+			UserProfile profile = user.getProfile();
+			ProfileImage profileImage = new ProfileImage(multipartImage.getBytes(), multipartImage.getOriginalFilename(), profile);
+			profile.setProfileImage(profileImage);
+			Long id = profileImageRepository.save(profileImage).getId();
+			userRepository.save(user);
+			return id;
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ByteArrayResource downloadProfileImage(Long userId)
+	{
+		Optional<User> user = userRepository.findById(userId);
+		if (user.isPresent())
+		{
+			UserProfile profile = user.get().getProfile();
+			ProfileImage image = profile.getProfileImage();
+			if(image == null)
+				return null;
+			byte[] imageBytes = image.getContent();
 			return new ByteArrayResource(imageBytes);
 		}
 		return null;
