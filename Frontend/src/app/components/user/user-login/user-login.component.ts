8dd797e5908706f8e77ebea421e8a913e77af962
Hadi <hadidanial97@gmail.com>
2022-02-21 07:08:46 +0200

    User Login now uses forms

--- Frontend/src/app/components/user/user-login/user-login.component.ts
+++ Frontend/src/app/components/user/user-login/user-login.component.ts
@@ -5,7 +5,7 @@ import { User } from 'src/app/models/entities/User';
 import { LoginRequest } from 'src/app/models/requests/LoginRequest';
 import { AuthService } from 'src/app/services/authService/auth.service';
 import { UserService } from 'src/app/services/userService/user.service';
-import {FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
@@ -14,86 +14,73 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class UserLoginComponent implements OnInit, OnDestroy
 {
-  password:string="";
-  username:string="";
   isLoading = false;
   isLoggedIn = false;
   triedToLogin = false;
   defaultMessage = "Please enter your username and password to sign in.";
   successfulLoginMessage = "Signed in successfully. Welcome back, ";
   failedLoginMessage = "Failed to sign in. Please try again.";
+  disallowedUsernameMessage:string = 'Username must start with a letter and only contain letters and numbers.'
   currentMessage = "";
   userSubscription: Subscription = new Subscription();
-  currentUser : User = this.userService.user;
-  firstName = "";
-  lastName = "";
-  email = "";
+  currentUser: User = this.userService.user;
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  
-  constructor(private userService: UserService, private authService:AuthService, private router: Router) { }
+  form = new FormGroup({});
+  minUsernameLen: number = 3;
+  maxUsernameLen: number = 20;
+  constructor(private userService: UserService, private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
 
   ngOnInit(): void
   {
+    this.form = this.formBuilder.group(
+      {
+        username: ["", { validators: [Validators.required, Validators.pattern(this.userService.allowedUsernamePattern),
+                                      Validators.minLength(this.minUsernameLen), Validators.maxLength(this.maxUsernameLen)] }],
+        password: ["", [Validators.required,  Validators.minLength(this.userService.minPassLen), 
+                        Validators.maxLength(this.userService.maxPassLen)]] //Validators.pattern(this.userService.passwordPattern),
+      })
     this.currentMessage = this.defaultMessage;
     this.userSubscription = this.userService.currentUser.subscribe(currentUser => 
+    {
+      this.currentUser = currentUser;
+      let equals = this.currentUser.equals(User.getDefaultUser());
+      this.isLoggedIn = (!currentUser.equals(User.getDefaultUser()));
+      if (!this.isLoggedIn && this.triedToLogin)
       {
-        this.currentUser = currentUser;
-        let equals = this.currentUser.equals(User.getDefaultUser());
-        this.isLoggedIn = (!currentUser.equals(User.getDefaultUser()));
-        if(!this.isLoggedIn && this.triedToLogin)
+        if (equals) // Error
         {
-          if(equals) // Error
-          {               
-            this.currentMessage = this.failedLoginMessage;        
-          }
-          else
-          {
-            this.currentMessage = this.successfulLoginMessage + this.currentUser.getName() + "!";
-            this.isLoggedIn = true;
-            this.password="";
-            this.router.navigate(['/home']);
-            // TODO: Redirect to home
-          }
-
+          this.currentMessage = this.failedLoginMessage;
         }
         else
         {
-          if(!equals)
-          {
-            console.log("Not equals");
-          }
+          this.currentMessage = this.successfulLoginMessage + this.currentUser.getName() + "!";
+          this.isLoggedIn = true;
+          this.router.navigate(['/home']);
+          // TODO: Redirect to home
         }
-      })
+
+      }
+    })
   }
   ngOnDestroy(): void
   {
     this.userSubscription.unsubscribe();
   }
 
-  updateUsername(username: any)  
+  onSubmit()
   {
-    this.username = username.target.value;
+    console.log(this.form)
+    if(this.form.valid)
+    {
+      let req = new LoginRequest(this.form.value["username"],this.form.value["password"]);
+      this.triedToLogin = true;
+      this.authService.login(req);
+    }
   }
-  updatePassword(password: any)  
+  
+  getFormControl(controlName: string): FormControl
   {
-    this.password = password.target.value;
-  }
-
-  login()
-  {
-    let req = new LoginRequest(this.username, this.password);
-    this.triedToLogin = true;
-    this.authService.login(req);
-  }
-
-  logout()
-  {
-    this.authService.logout();
-  }
-
-  getAllUsers()
-  {
-    this.userService.getAllUsers();
+    return this.form.get(controlName) as FormControl;
   }
 }
