d10a124c3c0ff09d75a54646ab80dd180ca54d70
Hadi <hadidanial97@gmail.com>
2022-02-21 07:14:54 +0200

    Update app-routing.module.ts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserLoginComponent } from './components/user/user-login/user-login.component';
 import { UserSignupComponent } from './components/user/user-signup/user-signup.component';
+import { LoggedOutGuard } from './guards/logged-out.guard';
+import { LoginGuard } from './guards/login.guard';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { LogoutComponent } from './components/user/logout/logout.component';
+import { SearchComponent } from './components/general/search/search.component';
 
 const routes: Routes = [
   {
@@ -10,17 +15,23 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'logout',
+    component: LogoutComponent,
+    canActivate: [LoginGuard],
+  },
+  {
     path: 'login',
     component: UserLoginComponent,
+    canActivate: [LoggedOutGuard]
   },
   {
     path: 'signup',
     component: UserSignupComponent,
+    canActivate: [LoggedOutGuard]
   },
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path:'search',
+    component: SearchComponent
   },
   {
     path: '**',
