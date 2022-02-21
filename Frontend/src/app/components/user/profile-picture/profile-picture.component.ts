6cd918a308eb7cec3e9aea45be0608208b6fac88
Hadi <hadidanial97@gmail.com>
2022-02-22 00:35:04 +0200

    Profile picture now loads from server with custom css

--- Frontend/src/app/components/user/profile-picture/profile-picture.component.ts
+++ Frontend/src/app/components/user/profile-picture/profile-picture.component.ts
@@ -1,31 +1,57 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FileService } from 'src/app/services/fileService/file.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profile-picture',
   templateUrl: './profile-picture.component.html',
   styleUrls: ['./profile-picture.component.scss']
 })
-export class ProfilePictureComponent implements OnInit {
+export class ProfilePictureComponent implements OnInit
+{
 
-  constructor(private fileService: FileService) { }
+  constructor(private fileService: FileService, private sanitizer: DomSanitizer) { }
   @Input()
-  userId: number = 1;
+  userId: number = 0;
   blobData: Blob = new Blob();
   hasError = false;
   loading = true;
   profilePicHandler = this.fileService.profileImageHandler;
-  ngOnInit(): void {
+  @Input()
+  size: string = "100";
+  @Input()
+  borderRadius = "border-radius: 50%;"
+  @Input()
+  borderWidth = "border-width: 5px;"
+  @Input()
+  borderStyle = "border-style: solid;"
+  @Input()
+  fit = "object-fit: cover;"
+  width = "width: " + this.size + "px; height: " + this.size + "px;";
+  style = "";
+  imageSrc: any;
+
+  ngOnInit(): void
+  {
+    this.style = this.fit + this.width + this.borderRadius + this.borderWidth + this.borderStyle;
   }
   onGetImage(blob: Blob)
   {
     this.blobData = blob;
     this.loading = false;
+    this.createImageFromBlob(blob);
   }
-  
+
   onError()
   {
     this.hasError = true;
     this.loading = false;
+  }
+
+
+  createImageFromBlob(image: Blob)
+  {
+    let objectURL = URL.createObjectURL(image);
+    this.imageSrc = this.sanitizer.bypassSecurityTrustUrl(objectURL);
   }
 }
