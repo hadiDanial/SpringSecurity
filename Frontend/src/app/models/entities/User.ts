8d6b260c1bb9a7624089056a5a66139cc3e9d6a3
Hadi <hadidanial97@gmail.com>
2022-02-22 00:33:51 +0200

    Added profile to user

--- Frontend/src/app/models/entities/User.ts
+++ Frontend/src/app/models/entities/User.ts
@@ -1,22 +1,24 @@
 import { Name } from "../other/Name";
 import { Role } from "./Role";
-
+import { UserProfile } from "./UserProfile"
 export class User
 {
-    private static defaultUser = new User(0, "Guest", "",new Name("Guest",""), []);
+    private static defaultUser = new User(0, "Guest", "",new Name("Guest",""), [], new UserProfile("",[],[]));
     private _id: number;
     private _username: string;
     private _email: string;
     private _name: Name;
     private _roles: Role[];
+    private _profile: UserProfile;
 
-    constructor(id: number, username: string, email: string, name: Name, roles: Role[]) 
+    constructor(id: number, username: string, email: string, name: Name, roles: Role[], profile: UserProfile) 
     {
         this._id = id;
         this._username = username;
         this._email = email;
         this._name = name;
         this._roles = roles;
+        this._profile = profile;
     }
     public get id(): number
     {
@@ -68,6 +70,22 @@ export class User
 
 	public set roles(value: Role[] ) {
 		this._roles = value;
+	}
+
+    /**
+     * Getter profile
+     * @return {UserProfile}
+     */
+	public get profile(): UserProfile {
+		return this._profile;
+	}
+
+    /**
+     * Setter profile
+     * @param {UserProfile} value
+     */
+	public set profile(value: UserProfile) {
+		this._profile = value;
 	}
 
     static getDefaultUser(): User
