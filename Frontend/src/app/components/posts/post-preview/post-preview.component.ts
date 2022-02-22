5750a2047eb2bf50712656905d34dfdd6af46696
Hadi <hadidanial97@gmail.com>
2022-02-23 00:23:02 +0200

    Added empty post + Post Preview Component placeholder

--- Frontend/src/app/components/posts/post-preview/post-preview.component.ts
+++ Frontend/src/app/components/posts/post-preview/post-preview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/entities/Post';
 
 @Component({
   selector: 'app-post-preview',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class PostPreviewComponent implements OnInit {
 
   constructor() { }
-
+  @Input()
+  post: Post = Post.getEmptyPost();
   ngOnInit(): void {
   }
 
--- Frontend/src/app/models/entities/Post.ts
+++ Frontend/src/app/models/entities/Post.ts
@@ -1,33 +1,41 @@
 import { Comment } from "./Comment";
 import { Content } from "./Content";
+import { User } from "./User";
 import { UserProfile } from "./UserProfile";
 
 export class Post extends Content
 {
+  
     private _comments: Comment[];
 
-	constructor(id:  number, title:  string, text:  string, markdown:  string, releaseDate:  Date, lastEditDate:  Date, 
-        hasBeenEdited:  boolean, profile:  UserProfile, comments: Comment[]) 
-    {	
+    constructor(id: number, title: string, text: string, markdown: string, releaseDate: Date, lastEditDate: Date,
+        hasBeenEdited: boolean, profile: UserProfile, comments: Comment[]) 
+    {
         super(id, title, text, markdown, releaseDate, lastEditDate, hasBeenEdited, profile);
         this._comments = comments;
-	}
-
+    }
+    static getEmptyPost(): Post
+    {
+        let date = new Date();
+        return new Post(0, "Test", "This is my test text", "This is my **test** text", date, date, false, User.getDefaultUser().profile, []);
+    }
 
     /**
      * Getter comments
      * @return {Comment[]}
      */
-	public get comments(): Comment[] {
-		return this._comments;
-	}
+    public get comments(): Comment[]
+    {
+        return this._comments;
+    }
 
     /**
      * Setter comments
      * @param {Comment[]} value
      */
-	public set comments(value: Comment[]) {
-		this._comments = value;
-	}
+    public set comments(value: Comment[])
+    {
+        this._comments = value;
+    }
 
 }
