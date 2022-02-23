82c197f337a4ad3ec558b8fd501472918bd38cf3
Hadi <hadidanial97@gmail.com>
2022-02-23 13:56:41 +0200

    Post component

--- Frontend/src/app/components/posts/post/post.component.ts
+++ Frontend/src/app/components/posts/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/entities/Post';
 
 @Component({
   selector: 'app-post',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
 
   constructor() { }
-
+  @Input()
+  post: Post = Post.getEmptyPost();
+  
   ngOnInit(): void {
   }
 
--- Frontend/src/app/models/entities/UserProfile.ts
+++ Frontend/src/app/models/entities/UserProfile.ts
@@ -1,76 +1,108 @@
 import { Post } from "./Post";
 import { Comment } from "./Comment"
+import { User } from "./User";
 
 export class UserProfile
 {
     private _about: string;
     private _comments: Comment[];
     private _posts: Post[];
+    private _user: User | undefined;
     private _profileImage: Blob | undefined;
 
 
-	constructor(about: string, comments: Comment[], posts: Post[], profileImage?: Blob) {
-		this._about = about;
-		this._comments = comments;
-		this._posts = posts;
-		this._profileImage = profileImage;
-	}
+    constructor(about: string, comments: Comment[], posts: Post[], user?: User, profileImage?: Blob)
+    {
+        this._about = about;
+        this._comments = comments;
+        this._posts = posts;
+        this._user = user;
+        this._profileImage = profileImage;
+    }
 
 
     /**
      * Getter about
      * @return {string}
      */
-	public get about(): string {
-		return this._about;
-	}
+    public get about(): string
+    {
+        return this._about;
+    }
 
     /**
      * Setter about
      * @param {string} value
      */
-	public set about(value: string) {
-		this._about = value;
-	}
+    public set about(value: string)
+    {
+        this._about = value;
+    }
 
     /**
      * Getter comments
      * @return {Comment[]}
      */
-	public get comments(): Comment[] {
-		return this._comments;
-	}
+    public get comments(): Comment[]
+    {
+        return this._comments;
+    }
 
     /**
      * Setter comments
      * @param {Comment[]} value
      */
-	public set comments(value: Comment[]) {
-		this._comments = value;
-	}
+    public set comments(value: Comment[])
+    {
+        this._comments = value;
+    }
 
     /**
      * Getter posts
      * @return {Post[]}
      */
-	public get posts(): Post[] {
-		return this._posts;
-	}
+    public get posts(): Post[]
+    {
+        return this._posts;
+    }
 
     /**
      * Setter posts
      * @param {Post[]} value
      */
-	public set posts(value: Post[]) {
-		this._posts = value;
-	}
+    public set posts(value: Post[])
+    {
+        this._posts = value;
+    }
+
+    /**
+     * Getter user
+     * @return {User }
+     */
+    public get user(): User
+    {
+        if (this._user == undefined) 
+            return User.getDefaultUser();
+        else
+            return this._user;
+    }
+
+    /**
+     * Setter user
+     * @param {User } value
+     */
+    public set user(value: User)
+    {
+        this._user = value;
+    }
 
     /**
      * Setter profileImage
      * @param {Blob } value
      */
-	public set profileImage(value: Blob ) {
-		this._profileImage = value;
-	}
+    public set profileImage(value: Blob)
+    {
+        this._profileImage = value;
+    }
 
 }
